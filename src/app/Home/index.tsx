39d5acd28c3de8cf09db2f0321e44a15a142fe33
Rory Pearson <52637194+Mezmerizxd@users.noreply.github.com>
2022-10-22 22:42:41 +0100

    fixed messages not loading

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,11 +1,11 @@
 // Dependencies
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 // Classes
-import Api from "../../classes/Api";
+import Api from '../../classes/Api';
 
 // Styles
-import "./styles.scss";
+import './styles.scss';
 
 const Home = () => {
     const [updates, setUpdates] = useState(null);
@@ -14,8 +14,7 @@ const Home = () => {
         setTimeout(async () => {
             const response = await Api.POST('/services/check-for-updates');
             if (response?.data) {
-                setUpdates(JSON.parse(response.data))
-                console.log(response)
+                setUpdates(JSON.parse(response.data));
             }
         });
     }, []);
@@ -24,14 +23,18 @@ const Home = () => {
         <div className="Home-container">
             <div className="Updates-container">
                 <h1>Recent Updates</h1>
-                {
-                    updates && updates.length > 0 && (
-                        updates.map((update, i) => (
-                            <div className="Update" key={i}>
-                                <p className="Update-repo"><a target="_blank" href={update.repoUrl}>{update.repo}</a></p>
-                                <p className="Update-pusher">- {update.pusher}</p>
-                                {JSON.stringify(update.commits, null, 4)}
-                                {/* {
+                {updates &&
+                    updates.length > 0 &&
+                    updates.map((update, i) => (
+                        <div className="Update" key={i}>
+                            <p className="Update-repo">
+                                <a target="_blank" href={update.repoUrl}>
+                                    {update.repo}
+                                </a>
+                            </p>
+                            <p className="Update-pusher">- {update.pusher}</p>
+                            {JSON.stringify(update.commits, null, 4)}
+                            {/* {
                                     update.commits && update.commits.length > 0 && (
                                         update.commits.map((commit, i) => (
                                             <div className="Update-commit" key={i}>
@@ -43,14 +46,11 @@ const Home = () => {
                                         ))
                                     )
                                 } */}
-                            </div>
-                        ))
-                    )
-
-                }
+                        </div>
+                    ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Home;
